ibuclaw/dmd-monorepo

commit af948c1055b7b9468ae5e63db9c63c2fdf297d64

Add version (Windows): to Windows headers.

src/druntime/src/core/sys/windows/windows.d
@@ -14,8 +14,9 @@
  */
 module core.sys.windows.windows;
 
-
-extern (Windows) nothrow:
+version (Windows):
+extern (Windows)
+nothrow:
 
     alias uint ULONG;
     alias ULONG *PULONG;
@@ -140,7 +141,7 @@ else // Win32
     alias DWORD   *LPCOLORREF;
     alias WORD    ATOM;
 
-version (0)
+version (none)
 {   // Properly prototyped versions
     alias BOOL function(HWND, UINT, WPARAM, LPARAM) DLGPROC;
     alias VOID function(HWND, UINT, UINT_PTR, DWORD) TIMERPROC;

src/druntime/src/core/sys/windows/dbghelp.d
@@ -14,9 +14,9 @@
  */
 module core.sys.windows.dbghelp;
 
+version (Windows):
 
 import core.sys.windows.windows;
-
 
 alias CHAR TCHAR;
 

commit eb179994e09f8465e681ddb6492dfde81eaa9a60

Fix a version statement broken in af948c1055b7b9468ae5e63db9c63c2fdf297d64.

src/druntime/src/core/sys/windows/windows.d
@@ -141,7 +141,7 @@ else // Win32
     alias DWORD   *LPCOLORREF;
     alias WORD    ATOM;
 
-version (none)
+version (all)
 {   // Properly prototyped versions
     alias BOOL function(HWND, UINT, WPARAM, LPARAM) DLGPROC;
     alias VOID function(HWND, UINT, UINT_PTR, DWORD) TIMERPROC;