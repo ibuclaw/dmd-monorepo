ibuclaw/dmd-monorepo

commit 95c4444845799370a386d097e7adb19a939cbd8f

Some large file support enhancements
The normal functions should be aliases to the *64 functions,
even if _LARGEFILE64_SOURCE is false. _LARGEFILE64_SOURCE is
more or less deprecated.

src/druntime/src/core/sys/posix/sys/statvfs.d
@@ -93,5 +93,16 @@ else
     }
 }
 
-int statvfs (const char * file, statvfs_t* buf);
-int fstatvfs (int fildes, statvfs_t *buf); 
+static if( __USE_FILE_OFFSET64 )
+{
+	int statvfs64 (const char * file, statvfs_t* buf);
+	alias statvfs64 statvfs;
+
+	int fstatvfs64 (int fildes, statvfs_t *buf);
+	alias fstatvfs64 fstatvfs;
+}
+else
+{
+	int statvfs (const char * file, statvfs_t* buf);
+	int fstatvfs (int fildes, statvfs_t *buf);
+}

src/druntime/src/core/sys/posix/dirent.d
@@ -69,7 +69,7 @@ version( linux )
         // Managed by OS
     }
 
-    static if( __USE_LARGEFILE64 )
+    static if( __USE_FILE_OFFSET64 )
     {
         dirent* readdir64(DIR*);
         alias   readdir64 readdir;

src/druntime/src/core/sys/posix/fcntl.d
@@ -126,7 +126,7 @@ version( linux )
         pid_t   l_pid;
     }
 
-    static if( __USE_LARGEFILE64 )
+    static if( __USE_FILE_OFFSET64 )
     {
         int   creat64(in char*, mode_t);
         alias creat64 creat;

src/druntime/src/core/sys/posix/stdio.d
@@ -95,7 +95,12 @@ int    vsscanf(in char*, in char*, va_list arg);
 
 version( linux )
 {
-    static if( __USE_LARGEFILE64 )
+    /*
+     * actually, if __USE_FILE_OFFSET64 && !_LARGEFILE64_SOURCE
+     * the *64 functions shouldn't be visible, but the aliases should
+     * still be supported
+     */
+    static if( __USE_FILE_OFFSET64 )
     {
         int   fgetpos64(FILE*, fpos_t *);
         alias fgetpos64 fgetpos;
@@ -106,8 +111,7 @@ version( linux )
         FILE* freopen64(in char*, in char*, FILE*);
         alias freopen64 freopen;
 
-        int   fseek64(FILE*, c_long, int);
-        alias fseek64 fseek;
+        int   fseek(FILE*, c_long, int);
 
         int   fsetpos64(FILE*, in fpos_t*);
         alias fsetpos64 fsetpos;
@@ -156,7 +160,7 @@ version( linux )
     int   fseeko(FILE*, off_t, int);
   }
 
-  static if( __USE_LARGEFILE64 )
+  static if( __USE_FILE_OFFSET64 )
   {
     off_t ftello64(FILE*);
     alias ftello64 ftello;

src/druntime/src/core/sys/posix/sys/mman.d
@@ -132,7 +132,7 @@ version( linux )
     //void* mmap(void*, size_t, int, int, int, off_t);
     int   munmap(void*, size_t);
 
-  static if( __USE_LARGEFILE64 )
+  static if( __USE_FILE_OFFSET64 )
   {
     void* mmap64(void*, size_t, int, int, int, off_t);
     alias mmap64 mmap;

src/druntime/src/core/sys/posix/unistd.d
@@ -548,7 +548,7 @@ version( linux )
     int        usleep(useconds_t);
     pid_t      vfork();
 
-  static if( __USE_LARGEFILE64 )
+  static if( __USE_FILE_OFFSET64 )
   {
     int        lockf64(int, int, off_t);
     alias      lockf64 lockf;

commit 8b8f2b51624e470f9428a85fb2da6950a04e72c0

Fixed 2.062 OS X regression introduced by 95c44448.
This reverts the module to the pre-95c44448 state for
non-Linux. It should probably be amended to follow the
same conventions as for other module files, which might
help to avoid such problems.

src/druntime/src/core/sys/posix/sys/statvfs.d
@@ -68,6 +68,21 @@ version(linux) {
             ST_NOSUID = 2
         }
     }
+
+    static if( __USE_FILE_OFFSET64 )
+    {
+        int statvfs64 (const char * file, statvfs_t* buf);
+        alias statvfs64 statvfs;
+
+        int fstatvfs64 (int fildes, statvfs_t *buf);
+        alias fstatvfs64 fstatvfs;
+    }
+    else
+    {
+        int statvfs (const char * file, statvfs_t* buf);
+        int fstatvfs (int fildes, statvfs_t *buf);
+    }
+
 }
 else
 {
@@ -91,18 +106,7 @@ else
         ST_RDONLY = 1,        /* Mount read-only.  */
         ST_NOSUID = 2
     }
-}
 
-static if( __USE_FILE_OFFSET64 )
-{
-	int statvfs64 (const char * file, statvfs_t* buf);
-	alias statvfs64 statvfs;
-
-	int fstatvfs64 (int fildes, statvfs_t *buf);
-	alias fstatvfs64 fstatvfs;
-}
-else
-{
-	int statvfs (const char * file, statvfs_t* buf);
-	int fstatvfs (int fildes, statvfs_t *buf);
+    int statvfs (const char * file, statvfs_t* buf);
+    int fstatvfs (int fildes, statvfs_t *buf);
 }